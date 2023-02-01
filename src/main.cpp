53da96e6a8f24efb4a48ee80b10f1695c258df9b
gsushil <gaurav7101879@gmail.com>
2023-02-01 21:40:50 +0530

    used rand() & srand() to generate random word from list of words

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,34 @@
 #include "../include/main.hpp"
 #include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <string>
+#include <time.h>
+//********* word guessing ***********
+
+// need to have a set of words to choose from
+
+std::vector<std::string> words = {
+"test", "taste", "book", "code", "program", "last"
+};
+
+
+//need a random function to pick a random word from "words"
+std::string pickWord(std::vector<std::string> &words){
+
+int limit = words.size()-1;
+
+//std::cout << "\t\t random picked index is " << rand()%limit << "\n";
+
+return words[std::rand() % limit]; // this rand() % limit return a random number in the range of 0 to limit 
+}
+
 
 int main()
 {
-	std::cout << "this is the first line to printed in the this project\n";
+	srand(time(0));
+	std::cout << "\n\t\t********* word guessing ************\n\n";
+
+	std::cout << pickWord(words) << "\n";
 	return 0;
 }
